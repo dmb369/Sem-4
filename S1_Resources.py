a184c6793be51df8e3b521c8da2e61d542599af2
Dev M. Bandhiya <116587999+dmb369@users.noreply.github.com>
2024-01-29 17:21:50 +0530

    The correct code

--- S1_Resources.py
+++ S1_Resources.py
@@ -3,7 +3,20 @@
 n = int(input("Enter the number of individuals:"))
 
 resources=[]
-for i in range(n):
-    resources[i]=int(input("Enter the amount",i,"th indivual has:\n"))
 
-print(resources)
+for i in range(0,n):
+    l=int(input())
+    resources.append(l)
+
+extra = int(input("Enter the amount of extra resources:"))
+m=max(resources)
+result=[]
+
+for i in range(0,n):
+    k=resources[i]+extra
+    if (k>=m):
+        result.append(True)
+    else:
+        result.append(False)
+
+print(result)
