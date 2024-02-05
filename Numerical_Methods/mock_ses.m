46bf693ee5285f71eef9234d303f1c0510de7c8f
Dev M. Bandhiya <116587999+dmb369@users.noreply.github.com>
2024-02-05 09:46:32 +0530

    Exploring the world of MATLAB

---
+++ Numerical_Methods/mock_ses.m
@@ -0,0 +1,86 @@
+%-- 29/01/24, 9:12 AM --%
+clear ans
+y=9
+clear
+clc
+trig
+2*3
+2**3
+2^3
+(9^(9^9))
+2^(-99999)
+clc
+2^(-4)
+format long
+2^(-4)
+format double
+clc
+format short
+2^-3
+format long
+2^-4
+x=3^-2
+format short x
+format short
+x
+x = [1 2 3 4 5]
+a = [ 1 2 3 4 5 ]
+i=1
+for i in range(6):
+clc
+x=
+clc
+x=[1 2 3]
+x=[1 2 3]'
+x=[1 2 3; 4 5]
+x=[1; 2; 3; 4; 5;]
+x'
+y=[1 2 3;]
+k=[]
+m=[1 2 3; 4 5 6; 7 8 9;]
+m'
+m
+k
+a
+'''kfjgvdfj'''
+n=m=1
+m=1, n=m
+b=a+1
+b=b-2
+c=2*b
+c^3
+c=c.^3
+c(0,3)
+c(1,3)
+length(c)
+clc
+clear
+clc
+x=[1 2; 3 4]
+x[1 ;]
+x(1,;)
+x=(1,:)
+x(:,1)
+x(1,:)
+x(:,1)+1
+x
+a=[1 2 3 4 5];
+b=[a 5 6 7];
+b=[a 5 6 7]
+eye(100,100)
+eye(3,3)
+zeros(3,3)
+ones(1,1)
+x=ones(5,5)
+x>2
+a=1:16
+b=2:3
+c=2:2:100
+format long
+d
+x=-2:0.5:10;
+y=exp(x/2);
+plot(x,y)
+x=0:0.1:2*pi;
+y=sin(x);
+plot(x,y)
